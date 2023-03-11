43f75dec648245afac49c633d9265b84ff923d59
Fran <francinerodriguesmaia@gmail.com>
2023-03-11 21:24:38 +0000

    Creation of functions

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ sum.innerHTML = 0;
 square.innerHTML = 0;
 percentage.innerHTML = 0;
 
+function calculateSum(a, b) {
+    return a + b;
+}
 
+function calculateSquare(a) {
+    return Math.sqrt(a).toFixed(2);
+}
+
+function calculatePercentage(a, b) {
+    return ((a * 100)/b).toFixed(0);
+}
+
+function calculate() {
+    const inputA = document.getElementById("firstNumber");
+    const inputB = document.getElementById("secondNumber");     
+}
